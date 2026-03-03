bd5cfacf1fe19056c0d60e0f0a75dfc6ee8f4f5f
chych-a <dedream1337@gmail.com>
2026-03-03 11:58:34 +0100

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,18 +3,31 @@ def analyze_log(file_name):
         lines = file.readlines()
 
     errors = []
+    fail_count = 0
+    error_count = 0
 
     for line in lines:
         line = line.strip()
 
-        if line.startswith("FAIL") or line.startswith("ERROR"):
+        if line.startswith("FAIL"):
             errors.append(line)
+            fail_count += 1
 
-    return errors
+        elif line.startswith("ERROR"):
+            errors.append(line)
+            error_count += 1
+
+    return errors, fail_count, error_count
 
 
-log_errors = analyze_log("sample.log")
+log_errors, fail_count, error_count = analyze_log("sample.log")
 
 print("Znalezione błędy:")
 for error in log_errors:
     print(error)
+
+print()
+print("Podsumowanie:")
+print("Liczba FAIL:", fail_count)
+print("Liczba ERROR:", error_count)
+print("Łączna liczba błędów:", fail_count + error_count)
