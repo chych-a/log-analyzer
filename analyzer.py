92649c8dc8e43a0ff36ab3263138ac94014dbef8
chych-a <dedream1337@gmail.com>
2026-03-03 11:52:02 +0100

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,20 @@
+def analyze_log(file_name):
+    with open(file_name, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
+    errors = []
+
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("FAIL") or line.startswith("ERROR"):
+            errors.append(line)
+
+    return errors
+
+
+log_errors = analyze_log("sample.log")
+
+print("Znalezione błędy:")
+for error in log_errors:
+    print(error)
